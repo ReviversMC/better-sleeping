edbf01c3e7e65515fb977fcae91b13a312ab3c20
NebelNidas <nebelnidas@gmail.com>
2024-06-25 11:28:46 +0200

    Add 1.21 support

--- bettersleeping-1.20.5/src/main/java/com/github/reviversmc/bettersleeping/events/EventHandler1205.java
+++ bettersleeping-1.20.5/src/main/java/com/github/reviversmc/bettersleeping/events/EventHandler1205.java
@@ -8,11 +8,11 @@ import net.minecraft.util.Identifier;
 public class EventHandler1205 extends EventHandler1193 {
 	@Override
 	protected StatusEffectInstance newStatusEffectInstance(String id, int duration, int amplifier) {
-		return new StatusEffectInstance(Registries.STATUS_EFFECT.getEntry(new Identifier(id)).get(), duration, amplifier);
+		return new StatusEffectInstance(Registries.STATUS_EFFECT.getEntry(Identifier.tryParse(id)).get(), duration, amplifier);
 	}
 
 	@Override
 	protected void removeStatusEffect(ServerPlayerEntity player, String id) {
-		player.removeStatusEffect(Registries.STATUS_EFFECT.getEntry(new Identifier(id)).get());
+		player.removeStatusEffect(Registries.STATUS_EFFECT.getEntry(Identifier.tryParse(id)).get());
 	}
 }
